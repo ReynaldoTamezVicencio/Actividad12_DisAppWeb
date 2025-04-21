592ad46bcbb3e4de0d4cb9108081bbef9b27b59f
ReynaldoTamezVicencio <reynaldotamezvic@gmail.com>
2025-04-21 11:16:40 -0600

    Proyecto inicial Chuck Norris Jokes con Vue

---
+++ README.md
@@ -0,0 +1,18 @@
+# ChuckJokesVue
+
+## Descripción
+Proyecto en Vue.js que muestra frases graciosas de Chuck Norris. Se utilizan directivas como `v-for` para iterar y mostrar los datos en pantalla. Se aplican estilos minimalistas para mejor presentación.
+
+## Capturas de pantalla
+(Agrega aquí las imágenes del navegador con el resultado)
+
+## Tecnologías
+- HTML5
+- CSS3
+- Vue.js (CDN)
+
+## Autor
+Vicencio - Ingeniería en Desarrollo de Software
+
+## Enlace al proyecto
+https://github.com/TU_USUARIO/ChuckJokesVue
---
+++ app.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#app',
+    data: {
+      chuck: [
+        { value: "Chuck Norris puede saltar en paracaídas al espacio exterior." },
+        { value: "El principal producto de Chuck Norris es el dolor." },
+        { value: "Chuck Norris no lee libros. Los mira fijamente hasta que obtiene la información que quiere." },
+        { value: "El tiempo no espera a nadie. A menos que ese hombre sea Chuck Norris." },
+        { value: "Si escribes Chuck Norris en Scrabble, ganas. Para siempre." }
+      ]
+    }
+  });
+  
